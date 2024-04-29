d52cedb2d09eca4abc7a6b0eb1d7a1f1e776afef
Tuan Anh Nguyen <admin@Admins-MacBook-Pro.local>
2024-04-29 18:05:45 +0700

    Change reponsive and dark theme

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let navbar = document.querySelector('.navbar');
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
+
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
@@ -14,7 +15,7 @@ window.onscroll = () => {
             navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*='+id+']').classList.add('active');
-            })
+            });
         }
     })
 }
